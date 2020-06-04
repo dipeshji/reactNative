01dfd9e47eb60880342bdb6f33c3bc5949115f92
dipeji <dpatidar336@gmail.com>
2020-06-04 17:09:25 +0530

    new screens

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import IndexScreen from './src/screens/IndexScreen';
+import { BlogProvider } from './src/context/BlogContext';
+
+
+const navigator = createStackNavigator(
+  {
+    Index: IndexScreen
+  },
+  {
+    initialRouteName: 'Index',
+    defaultNavigationOptions: {
+      title: 'Blogs'
+    }
+  }
+);
+
+const App = createAppContainer(navigator);
+
+
+// BlogProvider is context which raps our app,
+//  that mean BlogProvider raps all the component that are in our app.
+export default () => {
+  return (
+    <BlogProvider>
+      <App />
+    </BlogProvider>
+  );
+};
+
